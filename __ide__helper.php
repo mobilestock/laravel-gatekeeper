e1290c5eab69309b4b20521c3f08d29731b90967
felprangel <feliperangel897@gmail.com>
2024-11-18 09:03:42 -0300

    adiciona ide helper

---
+++ __ide__helper.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Laravel\Socialite\Facades {
+    class Socialite
+    {
+        /**
+         * @return \Laravel\Socialite\Two\AbstractProvider
+         */
+        public static function driver(string $driver = null)
+        {
+            //
+        }
+    }
+}
