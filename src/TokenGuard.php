ee4f54c9d6c7f61477f690214f4298ed26a49636
felprangel <feliperangel897@gmail.com>
2025-01-30 15:52:46 -0300

    remove propriedades do construtor

--- src/TokenGuard.php
+++ src/TokenGuard.php
@@ -2,22 +2,15 @@
 
 namespace MobileStock\Gatekeeper;
 
-use Illuminate\Contracts\Auth\UserProvider as AuthUserProvider;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 
 class TokenGuard extends \Illuminate\Auth\TokenGuard
 {
-    public function __construct(
-        ?AuthUserProvider $provider,
-        Request $request,
-        $inputKey = 'id',
-        $storageKey = 'id',
-        $hash = false
-    ) {
+    public function __construct(Request $request, $inputKey = 'id', $storageKey = 'id', $hash = false)
+    {
         $this->hash = $hash;
         $this->request = $request;
-        $this->provider = $provider;
         $this->inputKey = $inputKey;
         $this->storageKey = $storageKey;
     }
