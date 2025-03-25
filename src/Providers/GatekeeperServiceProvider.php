ad430c7d5833da0ecb5261b2a6fc9abca7810595
felprangel <feliperangel897@gmail.com>
2025-03-25 16:39:08 -0300

    adiciona gate de admin

--- src/Providers/GatekeeperServiceProvider.php
+++ src/Providers/GatekeeperServiceProvider.php
@@ -4,9 +4,11 @@ namespace MobileStock\Gatekeeper\Providers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Socialite\Facades\Socialite;
+use MobileStock\Gatekeeper\Socialite\User;
 use MobileStock\Gatekeeper\Socialite\UsersProvider;
 use MobileStock\Gatekeeper\TokenGuard;
 
@@ -26,6 +28,10 @@ class GatekeeperServiceProvider extends ServiceProvider
 
         Config::set('services.users.front_url', Config::get('gatekeeper.users_front_url'));
         Config::set('services.users.api_url', Config::get('gatekeeper.users_api_url'));
+
+        Gate::define('admin', function (User $user) {
+            return $user->is_admin;
+        });
     }
 
     public function register(): void
