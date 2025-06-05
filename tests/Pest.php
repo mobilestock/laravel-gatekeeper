c48f16f89a0440fad6c94f593cc517488f8a91cb
felprangel <feliperangel897@gmail.com>
2025-06-05 14:34:03 -0300

    adiciona comentario da issue

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,6 +2,9 @@
 
 uses(Tests\TestCase::class)->in(__DIR__);
 
+/**
+ * @issue https://github.com/mobilestock/backend/issues/1001
+ */
 function invokeProtectedMethod(object $class, string $methodName, array $parameters = []): mixed
 {
     $method = new ReflectionMethod($class, $methodName);
