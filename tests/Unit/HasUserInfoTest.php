bac745d40eefd39aa063df664d248ade4dabbc79
felprangel <feliperangel897@gmail.com>
2025-06-12 14:06:16 -0300

    remove before each

--- tests/Unit/HasUserInfoTest.php
+++ tests/Unit/HasUserInfoTest.php
@@ -2,24 +2,21 @@
 
 use MobileStock\Gatekeeper\Traits\HasUserInfo;
 
-beforeEach(function () {
-    $this->class = new class {
+it('returns the correct user info', function () {
+    $class = new class {
         use HasUserInfo;
-
         public array $userInfo = [
             'id' => 1,
             'name' => 'John Doe',
             'email' => 'john@example.com',
         ];
     };
-});
 
-it('returns the correct user info', function () {
     $expected = [
         'id' => 1,
         'name' => 'John Doe',
         'email' => 'john@example.com',
     ];
 
-    expect($this->class->userInfo())->toBe($expected);
+    expect($class->userInfo())->toBe($expected);
 });
