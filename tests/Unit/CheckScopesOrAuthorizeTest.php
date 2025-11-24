7655c1559c3dacf2b73e45c4c5625a513a8061ea
SavioSReis <hjd44.sa@gmail.com>
2025-11-24 17:57:27 -0300

    feat: refatora teste de autenticação para reutilizar instância de CheckScopesOrAuthorize

--- tests/Unit/CheckScopesOrAuthorizeTest.php
+++ tests/Unit/CheckScopesOrAuthorizeTest.php
@@ -1,18 +1,20 @@
 <?php
 
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use MobileStock\Gatekeeper\Middleware\CheckScopesOrAuthorize;
+
+beforeEach(function () {
+    $this->middleware = new CheckScopesOrAuthorize();
+});
 
 it('should ensure token has required ability', function () {
     $abilities = ['SOME_ABILITY'];
     $gateSpy = Gate::spy();
     $gateSpy->shouldReceive('allows')->andReturnTrue();
 
-    invokeProtectedMethod(
-        new \MobileStock\Gatekeeper\Middleware\CheckScopesOrAuthorize(),
-        'ensureTokenHasRequiredAbility',
-        [$abilities]
-    );
+    invokeProtectedMethod($this->middleware, 'ensureTokenHasRequiredAbility', [$abilities]);
 
     $gateSpy->shouldHaveReceived('allows')->with($abilities)->once();
 });
@@ -22,9 +24,5 @@ it('should throw exception when token does not have required ability', function 
     $gateSpy = Gate::spy();
     $gateSpy->shouldReceive('allows')->andReturnFalse();
 
-    invokeProtectedMethod(
-        new \MobileStock\Gatekeeper\Middleware\CheckScopesOrAuthorize(),
-        'ensureTokenHasRequiredAbility',
-        [$abilities]
-    );
+    invokeProtectedMethod($this->middleware, 'ensureTokenHasRequiredAbility', [$abilities]);
 })->throws(AuthenticationException::class);
