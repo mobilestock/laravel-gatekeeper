d1a8adc9438677ae4d2d94012e03b0c433754d19
Gustavo210 <gustavogontijoo@gmail.com>
2026-05-06 10:58:52 -0300

    chore: colocando m2m dentro de users

--- src/Action/GetTokenM2MAction.php
+++ src/Action/GetTokenM2MAction.php
@@ -4,7 +4,6 @@ namespace MobileStock\Gatekeeper\Action;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Http;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class GetTokenM2MAction
 {
@@ -12,19 +11,18 @@ class GetTokenM2MAction
     {
         $defaultTtl = 60 * 60 * 24 * 365 * 100; // 100 years in seconds
         $cacheKey = Config::get('app.name') . '.' . Config::get('app.env') . '.m2m.token';
-        $ttl = Config::get('services.m2m.ttl', $defaultTtl);
+        $ttl = Config::get('services.users.m2m.ttl', $defaultTtl);
 
         $token = Cache::remember($cacheKey, $ttl, function () {
-            $response = Http::baseUrl(Config::get('gatekeeper.users_api_url'))->post('oauth/token', [
+            $baseUrl = Config::get('gatekeeper.users_api_url');
+            $payload = [
                 'grant_type' => 'client_credentials',
-                'client_id' => Config::get('services.m2m.client_id'),
-                'client_secret' => Config::get('services.m2m.client_secret'),
+                'client_id' => Config::get('services.users.m2m.client_id'),
+                'client_secret' => Config::get('services.users.m2m.client_secret'),
                 'scope' => '*',
-            ]);
+            ];
 
-            if (!$response->successful()) {
-                throw new BadRequestHttpException('Failed to retrieve M2M token');
-            }
+            $response = Http::baseUrl($baseUrl)->post('oauth/token', $payload);
 
             $accessToken = $response->json('access_token');
 
